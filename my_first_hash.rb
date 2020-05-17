05742698559fea088ad4bca0b21f8d6ead6ad1b7
Catheryne Small <catheryn.small@gmail.com>
2020-05-17 01:43:55 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -5,8 +5,8 @@ end
 
 def shipping_manifest
  the_manifest = { "whale bone corsets" => 5}
- the_manifest["procelain vases"] = 2
- the_manifest["oil paintings"] = 3
+ the_manifest["procelain vases"] = "2"
+ the_manifest["oil paintings"] = "3"
 end
 
 def retrieval
